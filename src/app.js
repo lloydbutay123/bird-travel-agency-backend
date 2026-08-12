a4465b96982cf540039bb351ca3efffce9be4c7a
lloydbutay123 <john.lloyd_butay@questronix.com.ph>
2026-08-13 02:45:04 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:3001",
       "https://bird-travel-agency-frontend.vercel.app",
-      "https://mytravelicious-travel-agency.vercel.app/",
+      "https://mytravelicious-travel-agency.vercel.app",
     ],
     credentials: true,
   }),
