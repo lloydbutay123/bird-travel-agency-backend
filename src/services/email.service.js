1d18633246bd50ec5a87d4ba04572bfb282aad62
lloydbutay123 <john.lloyd_butay@questronix.com.ph>
2026-08-13 13:35:09 +0800

    Update email.service.js

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -2,13 +2,16 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  family: 4,
+  port: 587,
+  secure: false,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
+  },
+
+  tls: {
+    family: 4,
   },
 });
 
